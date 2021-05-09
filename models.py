d7827a1b73c61598a0692649302750d9256708ef
Otmanzt <otmanzt@gmail.com>
2021-05-09 13:00:49 +0200

    Added the aggregatins functions to models and views

--- models.py
+++ models.py
@@ -10,3 +10,343 @@ def select_all():
 
     return result
 
+def listado_clientes():
+    result = client['Tienda']['Ventas_Cleaned'].aggregate([
+    {
+        '$group': {
+            '_id': {
+                'Customer_ID': '$Customer_ID', 
+                'Customer_name': '$Customer_Name', 
+                'Customer_segment': '$Customer_Segment'
+            }
+        }
+    }, {
+        '$project': {
+            '_id': 0, 
+            'customerID': '$_id.Customer_ID', 
+            'customerName': '$_id.Customer_name', 
+            'customerSegment': '$_id.Customer_segment'
+        }
+    }, {
+        '$sort': {
+            'customerID':1
+        }
+    }
+    ])
+    return result 
+
+def compras_categoria():
+    result = client['Tienda']['Ventas_Cleaned'].aggregate([
+    {
+        '$group': {
+            '_id': '$Category', 
+            'Num_sales': {
+                '$sum': 1
+            }
+        }
+    }, {
+        '$project': {
+            '_id': 0, 
+            'Category': '$_id', 
+            'Num_sales': 1
+        }
+    }, {
+        '$sort': {
+            'Category': 1
+        }
+    }
+    ])
+    return result
+
+def items_cliente_categoria():
+    result = client['Tienda']['Ventas_Cleaned'].aggregate([
+    {
+        '$project': {
+            'Category': 1, 
+            'Customer_Name': 1, 
+            'Item': 1, 
+            'Customer_ID': 1
+        }
+    }, {
+        '$group': {
+            '_id': {
+                'Customer_ID': '$Customer_ID', 
+                'Category': '$Category'
+            }, 
+            'Items': {
+                '$push': '$$ROOT'
+            }
+        }
+    }
+    ])
+    return result 
+
+def listado_productos_categoria():
+    result = client['Tienda']['Ventas_Cleaned'].aggregate([
+    {
+        '$project': {
+            'Category': 1, 
+            'Item': 1
+        }
+    }, {
+        '$group': {
+            '_id': {
+                'Category': '$Category'
+            }, 
+            'Items': {
+                '$push': '$$ROOT'
+            }
+        }
+    }
+    ])
+    return result
+
+def listado_productos_departamento():
+    result = client['Tienda']['Ventas_Cleaned'].aggregate([
+    {
+        '$project': {
+            'Department': 1, 
+            'Item': 1
+        }
+    }, {
+        '$group': {
+            '_id': {
+                'Departament': '$Department'
+            }, 
+            'Items': {
+                '$push': '$$ROOT'
+            }
+        }
+    }
+    ])
+    return result
+
+def productos_maxvendidos_categoria():
+    result = client['Tienda']['Ventas_Cleaned'].aggregate([
+    {
+        '$match': {
+            'Category': 'Storage & Organization'
+        }
+    }, {
+        '$group': {
+            '_id': {
+                'Item': '$Item'
+            }, 
+            'number': {
+                '$sum': 1
+            }
+        }
+    }, {
+        '$project': {
+            '_id': 0, 
+            'Item': '$_id.Item', 
+            'number': 1
+        }
+    }, {
+        '$sort': {
+            'number': -1
+        }
+    }, {
+        '$limit': 1
+    }
+    ])
+    return result
+
+def productos_maxvendidos_departamento():
+    result = client['Tienda']['Ventas_Cleaned'].aggregate([
+    {
+        '$match': {
+            'Department': 'Office Supplies'
+        }
+    }, {
+        '$group': {
+            '_id': {
+                'Item': '$Item'
+            }, 
+            'number': {
+                '$sum': 1
+            }
+        }
+    }, {
+        '$project': {
+            '_id': 0, 
+            'Item': '$_id.Item', 
+            'number': 1
+        }
+    }, {
+        '$sort': {
+            'number': -1
+        }
+    }, {
+        '$limit': 1
+    }
+    ])
+    return result
+
+def productos_maxcaros_categoria():
+    result = client['Tienda']['Ventas_Cleaned'].aggregate([
+    {
+        '$group': {
+            '_id': {
+                'Category': '$Category'
+            }, 
+            'Items': {
+                '$push': '$$ROOT'
+            }
+        }
+    }, {
+        '$sort': {
+            'Items.Unit Price': -1
+        }
+    }
+    ])
+    return result
+
+def productos_maxcaros_departamento():
+    result = client['Tienda']['Ventas_Cleaned'].aggregate([
+    {
+        '$group': {
+            '_id': {
+                'Department': '$Department'
+            }, 
+            'Items': {
+                '$push': '$$ROOT'
+            }
+        }
+    }, {
+        '$sort': {
+            'Items.Unit Price': -1
+        }
+    }
+    ])
+    return result
+
+def productos_maxvendidos_fecha():
+    result = client['Tienda']['Ventas_Cleaned'].aggregate([
+    {
+        '$addFields': {
+            'Fecha_ISO': {
+                '$dateFromString': {
+                    'dateString': '$Order_Date', 
+                    'format': '%d/%m/%Y'
+                }
+            }
+        }
+    }, {
+        '$match': {
+            'Fecha_ISO': {
+                '$gt': datetime(2011, 8, 11, 0, 0, 0, tzinfo=timezone.utc)
+            }
+        }
+    }, {
+        '$group': {
+            '_id': {
+                'Item': '$Item'
+            }, 
+            'number': {
+                '$sum': 1
+            }
+        }
+    }, {
+        '$project': {
+            '_id': 0, 
+            'Item': '$_id.Item', 
+            'number': 1
+        }
+    }, {
+        '$sort': {
+            'number': -1
+        }
+    }, {
+        '$limit': 1
+    }
+    ])
+    return result
+
+def items_comprado_cliente_fecha():
+    result = client['Tienda']['Ventas_Cleaned'].aggregate([
+    {
+        '$addFields': {
+            'Fecha_ISO': {
+                '$dateFromString': {
+                    'dateString': '$Order_Date', 
+                    'format': '%d/%m/%Y'
+                }
+            }
+        }
+    }, {
+        '$match': {
+            'Fecha_ISO': {
+                '$gt': datetime(2011, 8, 11, 0, 0, 0, tzinfo=timezone.utc)
+            }
+        }
+    }, {
+        '$group': {
+            '_id': {
+                'Customer_ID': '$Customer_ID'
+            }, 
+            'Items': {
+                '$push': '$$ROOT'
+            }
+        }
+    }, {
+        '$project': {
+            '_id': 0, 
+            'Customer_ID': '$_id.Customer_ID', 
+            'Items': 1
+        }
+    }, {
+        '$sort': {
+            'Customer_ID': -1
+        }
+    }
+    ])
+    return result
+
+def total_ventas_zona():
+    result = client['Tienda']['Ventas_Cleaned'].aggregate([
+    {
+        '$group': {
+            '_id': '$Country_Region', 
+            'Num_sales': {
+                '$sum': 1
+            }
+        }
+    }, {
+        '$project': {
+            '_id': 0, 
+            'Region': '$_id', 
+            'Num_sales': 1
+        }
+    }, {
+        '$sort': {
+            'Num_sales': -1
+        }
+    }
+    ])
+    return result
+
+def zona_max_compras():
+    result = client['Tienda']['Ventas_Cleaned'].aggregate([
+    {
+        '$group': {
+            '_id': '$Country_Region', 
+            'Num_sales': {
+                '$sum': 1
+            }
+        }
+    }, {
+        '$project': {
+            '_id': 0, 
+            'Region': '$_id', 
+            'Num_sales': 1
+        }
+    }, {
+        '$sort': {
+            'Num_sales': -1
+        }
+    }, {
+        '$limit': 3
+    }
+    ])
+    return result     
